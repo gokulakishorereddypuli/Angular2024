c19c0b5c3ce5b2fb6448112493dc3b572bdad0fb
PULI GOKULA KISHORE REDDY <puligokulakishorereddy@gmail.com>
2024-09-18 15:51:12 +0530

    exp8

---
+++ exp8_reading_data_from_server/src/app/fetch-data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FetchDataService {
+
+
+  constructor(private http:HttpClient) { } 
+  getUser(): Observable<any>{ 
+      return this.http.get("https://jsonplaceholder.typicode.com/users");  
+  }
+}
